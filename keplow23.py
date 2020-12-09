0ecc86a401733dca3962a76c15d243cbe340855a
remain89 <58208609+remain89@users.noreply.github.com>
2020-12-09 09:56:30 +0900

    c

--- keplow23.py
+++ keplow23.py
@@ -139,7 +139,8 @@ def glp(fname,tfile): #G,AE,S타입 LP검침
 
 list_of_files = sorted(glob.glob(os.getcwd()),key=os.path.getmtime) # octet-stream파일이 저장되는 경로를 지정
 latest_file = max(list_of_files, key=os.path.getctime)
-print(list_of_files)
+print(os.getcwd()+' getcwd()')
+print(list_of_files+' list_of_files')
 #print(list_of_files+'\nlocation-1\n')	 
 fantasy_zip = zipfile.ZipFile(latest_file)
 path =os.getcwd()+' LPdata '+str(datetime.today().year)+'-'+str(datetime.today().month)+'-'+str(datetime.today().day) # 결과가 저장될 폴더
